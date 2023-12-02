20b08458c588a697bb4ea7491d2801a9d5e2ec50
Stefan Hartnagel <hartnagel.stefan@web.de>
2023-12-02 10:28:40 +0100

    refactor & cleanup

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,25 +1,27 @@
 use std::{cmp::max, fs};
 
-fn get_power_of_games(games: &Vec<(u32, (u32, u32, u32))>) -> u32 {
-    let mut sum_power = 0;
-    for (_, colors) in games {
-        sum_power += colors.0 * colors.1 * colors.2;
-    }
-    return sum_power;
+fn get_sum_of_cube_power(games: &Vec<(u32, (u32, u32, u32))>) -> u32 {
+    return games
+        .iter()
+        .map(|(_, (red, green, blue))| red * green * blue)
+        .sum();
 }
 
-fn sum_possible_games(games: &Vec<(u32, (u32, u32, u32))>, max_colors: (u32, u32, u32)) -> u32 {
-    let mut sum = 0;
-    for (game_id, colors) in games {
-        if colors.0 <= max_colors.0 && colors.1 <= max_colors.1 && colors.2 <= max_colors.2 {
-            sum += game_id;
-        }
-    }
-    return sum;
+fn get_sum_of_possible_game_ids(
+    games: &Vec<(u32, (u32, u32, u32))>,
+    (max_red, max_green, max_blue): (u32, u32, u32),
+) -> u32 {
+    return games
+        .iter()
+        .filter(|(_, (red, green, blue))| {
+            red <= &max_red && green <= &max_green && blue <= &max_blue
+        })
+        .map(|(game_id, _)| game_id)
+        .sum();
 }
 
 fn parse_game(input: Vec<String>) -> Vec<(u32, (u32, u32, u32))> {
-    // id, red, green, blue
+    // id, (red, green, blue)
     let mut games: Vec<(u32, (u32, u32, u32))> = Vec::new();
     for line in input {
         let split: Vec<&str> = line.split(":").collect();
@@ -30,24 +32,22 @@ fn parse_game(input: Vec<String>) -> Vec<(u32, (u32, u32, u32))> {
             .unwrap()
             .parse::<u32>()
             .unwrap();
-        let games_string: Vec<&str> = split[1].split(";").collect();
-        let mut colors: (u32, u32, u32) = (0, 0, 0);
-        for mut game in games_string {
-            game = game.trim();
-            let colors_string: Vec<&str> = game.split(",").collect();
-            for mut color in colors_string {
-                color = color.trim();
+        let games_string: Vec<&str> = split[1].split(";").into_iter().map(|s| s.trim()).collect();
+        let (mut red, mut green, mut blue): (u32, u32, u32) = (0, 0, 0);
+        for game in games_string {
+            let colors_string: Vec<&str> = game.split(",").into_iter().map(|s| s.trim()).collect();
+            for color in colors_string {
                 let split_color: Vec<&str> = color.split(" ").collect();
                 let color_count = split_color[0].parse::<u32>().unwrap();
                 match split_color[1] {
-                    "red" => colors.0 = max(colors.0, color_count),
-                    "green" => colors.1 = max(colors.1, color_count),
-                    "blue" => colors.2 = max(colors.2, color_count),
+                    "red" => red = max(red, color_count),
+                    "green" => green = max(green, color_count),
+                    "blue" => blue = max(blue, color_count),
                     _ => panic!("color mismatch"),
                 }
             }
         }
-        games.push((game_id, colors));
+        games.push((game_id, (red, green, blue)));
     }
 
     return games;
@@ -57,7 +57,6 @@ fn get_input(file: &str) -> Vec<String> {
     let input = fs::read_to_string(file).expect("Should have been able to read the file");
     let lines: Vec<String> = input
         .split("\r\n")
-        .collect::<Vec<&str>>()
         .into_iter()
         .map(|s| s.to_owned())
         .collect();
@@ -67,8 +66,8 @@ fn get_input(file: &str) -> Vec<String> {
 pub fn solver() {
     let input = get_input("./src/day2/input.txt");
     let games = parse_game(input);
-    let sum_part_one = sum_possible_games(&games, (12, 13, 14));
-    let sum_part_two = get_power_of_games(&games);
+    let sum_part_one = get_sum_of_possible_game_ids(&games, (12, 13, 14));
+    let sum_part_two = get_sum_of_cube_power(&games);
     println!("Part 1: {sum_part_one}");
     println!("Part 2: {sum_part_two}");
 }
@@ -78,18 +77,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day2_simple_example_part_one() {
+    fn day2_example_input_part_one() {
         let input = get_input("./src/day2/example_input.txt");
         let games = parse_game(input);
-        let sum = sum_possible_games(&games, (12, 13, 14));
+        let sum = get_sum_of_possible_game_ids(&games, (12, 13, 14));
         assert_eq!(8, sum);
     }
 
     #[test]
-    fn day2_simple_example_part_two() {
+    fn day2_example_input_part_two() {
         let input = get_input("./src/day2/example_input.txt");
         let games = parse_game(input);
-        let power = get_power_of_games(&games);
+        let power = get_sum_of_cube_power(&games);
         assert_eq!(2286, power);
     }
 
@@ -97,16 +96,15 @@ mod tests {
     fn day2_input_part_one() {
         let input = get_input("./src/day2/input.txt");
         let games = parse_game(input);
-        let sum = sum_possible_games(&games, (12, 13, 14));
+        let sum = get_sum_of_possible_game_ids(&games, (12, 13, 14));
         assert_eq!(2268, sum);
     }
 
-    
     #[test]
     fn day2_input_part_two() {
         let input = get_input("./src/day2/input.txt");
         let games = parse_game(input);
-        let power = get_power_of_games(&games);
+        let power = get_sum_of_cube_power(&games);
         assert_eq!(63542, power);
     }
 }
