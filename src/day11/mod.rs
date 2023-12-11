2c90b7a6e9c2f8894c5f740591a69ea4ba3e0f47
Stefan Hartnagel <stefan.hartnagel@ul.com>
2023-12-11 10:08:50 +0100

    day11 replace part one with code from part two as it is more generalized

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -30,37 +30,6 @@ fn find_empty_space(universe: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
     (empty_rows, empty_cols)
 }
 
-fn expand_universe(universe: &Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut expanded_universe = universe.clone();
-    let mut inserted_rows = 0;
-    for row in 0..universe.len() {
-        if !universe[row].contains(&'#') {
-            expanded_universe.insert(row + inserted_rows, universe[row].clone());
-            inserted_rows += 1;
-        }
-    }
-
-    let mut inserted_cols = 0;
-    for col in 0..universe[0].len() {
-        let mut column_contains_universe = false;
-        for row in 0..universe.len() {
-            if universe[row][col] == '#' {
-                column_contains_universe = true;
-                break;
-            }
-        }
-        if column_contains_universe {
-            continue;
-        }
-        for row in 0..expanded_universe.len() {
-            expanded_universe[row].insert(col + inserted_cols, '.');
-        }
-        inserted_cols += 1;
-    }
-
-    expanded_universe
-}
-
 fn find_galaxies(universe: &Vec<Vec<char>>) -> Vec<(i128, i128)> {
     let mut galaxies: Vec<(i128, i128)> = Vec::new();
     for row in 0..universe.len() {
@@ -104,23 +73,11 @@ fn get_distance_between_galaxies(
     }
     let distance_without_empty_space =
         abs(start_galaxy.0 - end_galaxy.0) + abs(start_galaxy.1 - end_galaxy.1);
-    (distance_without_empty_space + (empty_rows_inbetween + empty_cols_inbetween) * (factor - 1)) as usize
+    (distance_without_empty_space + (empty_rows_inbetween + empty_cols_inbetween) * (factor - 1))
+        as usize
 }
 
-fn solve_part_one(universe: &Vec<Vec<char>>) -> usize {
-    let expanded_universe = expand_universe(universe);
-    let galaxies = find_galaxies(&expanded_universe);
-    let mut combined_distance = 0;
-    for i in 0..galaxies.len() {
-        for j in (i + 1)..galaxies.len() {
-            let distance = (abs(galaxies[i].0 - galaxies[j].0) + abs(galaxies[i].1 - galaxies[j].1)) as usize;
-            combined_distance +=distance;
-        }
-    }
-    combined_distance
-}
-
-fn solve_part_two(universe: &Vec<Vec<char>>, factor: i128) -> usize {
+fn solve(universe: &Vec<Vec<char>>, factor: i128) -> usize {
     let galaxies = find_galaxies(&universe);
     let (empty_rows, empty_cols) = find_empty_space(universe);
     let mut combined_distance = 0;
@@ -150,9 +107,9 @@ fn get_input(file: &str) -> Vec<Vec<char>> {
 
 pub fn solver() {
     let input = get_input("./src/day11/input.txt");
-    let sum_part_one = solve_part_one(&input);
+    let sum_part_one = solve(&input, 2);
     println!("Part 1: {sum_part_one}");
-    let sum_part_two = solve_part_two(&input, 1000000);
+    let sum_part_two = solve(&input, 1000000);
     println!("Part 2: {sum_part_two}");
 }
 
@@ -164,54 +121,47 @@ mod tests {
     #[test]
     fn day11_example_input_part_one() {
         let input = get_input("./src/day11/example_input.txt");
-        let sum_part_one = solve_part_one(&input);
+        let sum_part_one = solve(&input, 2);
         assert_eq!(374, sum_part_one);
     }
 
     #[test]
     fn day11_input_part_one() {
         let input = get_input("./src/day11/input.txt");
-        let sum_part_one = solve_part_one(&input);
+        let sum_part_one = solve(&input, 2);
         assert_eq!(9723824, sum_part_one);
-    }
-
-    #[test]
-    fn day11_example_input_part_two() {
-        let input = get_input("./src/day11/example_input.txt");
-        let sum_part_two = solve_part_two(&input, 2);
-        assert_eq!(374, sum_part_two);
     }
 
     #[test]
     fn day11_example_input_part_two_10() {
         let input = get_input("./src/day11/example_input.txt");
-        let sum_part_two = solve_part_two(&input, 10);
+        let sum_part_two = solve(&input, 10);
         assert_eq!(1030, sum_part_two);
     }
 
     #[test]
     fn day11_example_input_part_two_100() {
         let input = get_input("./src/day11/example_input.txt");
-        let sum_part_two = solve_part_two(&input, 100);
+        let sum_part_two = solve(&input, 100);
         assert_eq!(8410, sum_part_two);
     }
 
     #[test]
     fn day11_input_part_two() {
         let input = get_input("./src/day11/input.txt");
-        let sum_part_two = solve_part_two(&input, 1000000);
+        let sum_part_two = solve(&input, 1000000);
         assert_eq!(731244261352, sum_part_two);
     }
 
     #[bench]
     fn bench_part_one(b: &mut Bencher) {
         let input = get_input("./src/day11/input.txt");
-        b.iter(|| solve_part_one(&input))
+        b.iter(|| solve(&input, 2))
     }
 
     #[bench]
     fn bench_part_two(b: &mut Bencher) {
         let input = get_input("./src/day11/input.txt");
-        b.iter(|| solve_part_two(&input, 1000000))
+        b.iter(|| solve(&input, 1000000))
     }
 }
