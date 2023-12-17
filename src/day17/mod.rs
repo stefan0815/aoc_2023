cdfb105813d04524fa02a5fd329d90b88c0874aa
Stefan Hartnagel <hartnagel.stefan@web.de>
2023-12-17 13:24:05 +0100

    day17 part two

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -67,6 +67,71 @@ impl Pos {
             })
             .collect()
     }
+
+    fn ultra_successors(&self, layout: &Vec<Vec<i128>>, goal: (i128, i128)) -> Vec<(Pos, i128)> {
+        let mut next_positions: Vec<Pos> = Vec::new();
+        if self.straight_steps < 10 {
+            let next_pos = (
+                self.position.0 + self.direction.0,
+                self.position.1 + self.direction.1,
+            );
+            if in_bounds(layout, &next_pos) {
+                let straight_pos = Pos {
+                    position: next_pos,
+                    direction: self.direction,
+                    straight_steps: self.straight_steps + 1,
+                };
+                if next_pos.0 == goal.0 && next_pos.1 == goal.1 {
+                    if straight_pos.straight_steps > 4 {
+                        next_positions.push(straight_pos);
+                    }
+                } else {
+                    next_positions.push(straight_pos);
+                }
+            }
+        }
+
+        if self.straight_steps >= 4 {
+            let left_turn = (-self.direction.1, -self.direction.0);
+            let next_pos = (self.position.0 + left_turn.0, self.position.1 + left_turn.1);
+            if in_bounds(layout, &next_pos) {
+                let left_pos = Pos {
+                    position: next_pos,
+                    direction: left_turn,
+                    straight_steps: 1,
+                };
+                if !(next_pos.0 == goal.0 && next_pos.1 == goal.1) {
+                    next_positions.push(left_pos);
+                }
+            }
+
+            let right_turn = (self.direction.1, self.direction.0);
+            let next_pos = (
+                self.position.0 + right_turn.0,
+                self.position.1 + right_turn.1,
+            );
+            if in_bounds(layout, &next_pos) {
+                let right_pos = Pos {
+                    position: next_pos,
+                    direction: right_turn,
+                    straight_steps: 1,
+                };
+                if !(next_pos.0 == goal.0 && next_pos.1 == goal.1) {
+                    next_positions.push(right_pos);
+                }
+            }
+        }
+
+        next_positions
+            .iter()
+            .map(|p| {
+                (
+                    p.clone(),
+                    layout[p.position.0 as usize][p.position.1 as usize],
+                )
+            })
+            .collect()
+    }
 }
 
 fn in_bounds(height_map: &Vec<Vec<i128>>, position: &(i128, i128)) -> bool {
@@ -95,8 +160,33 @@ fn solve_part_one(layout: &Vec<Vec<i128>>) -> i128 {
     result.unwrap().1
 }
 
-fn solve_part_two(_: &Vec<Vec<i128>>) -> i128 {
-    0
+fn solve_part_two(layout: &Vec<Vec<i128>>) -> i128 {
+    let goal: (i128, i128) = (
+        (layout.len() - 1) as i128,
+        (layout[layout.len() - 1].len() - 1) as i128,
+    );
+    let start: Pos = Pos {
+        position: (0, 0),
+        direction: (0, 1),
+        straight_steps: 0,
+    };
+    let result = astar(
+        &start,
+        |p| p.ultra_successors(layout, goal),
+        |p| p.distance_simple(&goal) / 3,
+        |p| p.position == goal,
+    );
+    println!(
+        "{:?}",
+        result
+            .clone()
+            .unwrap()
+            .0
+            .iter()
+            .map(|pos| pos.position)
+            .collect::<Vec<(i128, i128)>>()
+    );
+    result.unwrap().1
 }
 
 fn get_input(file: &str) -> Vec<Vec<i128>> {
@@ -137,21 +227,28 @@ mod tests {
     fn day17_input_part_one() {
         let input = get_input("./src/day17/input.txt");
         let sum_part_one = solve_part_one(&input);
-        assert_eq!(932, sum_part_one);
+        assert_eq!(886, sum_part_one);
     }
 
     #[test]
     fn day17_example_input_part_two() {
         let input = get_input("./src/day17/example_input.txt");
         let sum_part_two = solve_part_two(&input);
-        assert_eq!(51, sum_part_two);
+        assert_eq!(94, sum_part_two);
+    }
+
+    #[test]
+    fn day17_example_input_two_part_two() {
+        let input = get_input("./src/day17/example_input_two.txt");
+        let sum_part_two = solve_part_two(&input);
+        assert_eq!(71, sum_part_two);
     }
 
     #[test]
     fn day17_input_part_two() {
         let input = get_input("./src/day17/input.txt");
         let sum_part_two = solve_part_two(&input);
-        assert_eq!(7793, sum_part_two);
+        assert_eq!(1049, sum_part_two);
     }
 
     #[bench]
