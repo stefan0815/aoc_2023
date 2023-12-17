d7b52ed7564af9c7a8b5db058c8366e5393bc710
Stefan Hartnagel <hartnagel.stefan@web.de>
2023-12-17 13:03:42 +0100

    day17 a star solution part one

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -1,42 +1,29 @@
 use pathfinding::prelude::astar;
-use std::{collections::HashMap, fs};
+use std::fs;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos {
-    layout: Vec<Vec<i128>>,
     position: (i128, i128),
     direction: (i128, i128),
-    heat_loss: i128,
-    heat_loss_so_far: i128,
     straight_steps: i128,
 }
 
 impl Pos {
-    fn distance(&self, other: &Pos) -> i128 {
-        (self.position.0.abs_diff(other.position.0) + self.position.1.abs_diff(other.position.1))
-            as i128
-    }
-
     fn distance_simple(&self, other: &(i128, i128)) -> i128 {
-        (self.position.0.abs_diff(other.0) + self.position.1.abs_diff(other.1))
-            as i128
+        (self.position.0.abs_diff(other.0) + self.position.1.abs_diff(other.1)) as i128
     }
 
-    fn successors(&self) -> Vec<(Pos, i128)> {
+    fn successors(&self, layout: &Vec<Vec<i128>>) -> Vec<(Pos, i128)> {
         let mut next_positions: Vec<Pos> = Vec::new();
         if self.straight_steps < 3 {
             let next_pos = (
                 self.position.0 + self.direction.0,
                 self.position.1 + self.direction.1,
             );
-            if in_bounds(&self.layout, &next_pos) {
-                let heat_loss: i128 = self.layout[next_pos.0 as usize][next_pos.1 as usize];
+            if in_bounds(layout, &next_pos) {
                 let straight_pos = Pos {
-                    layout: self.layout.to_vec(),
                     position: next_pos,
                     direction: self.direction,
-                    heat_loss,
-                    heat_loss_so_far: self.heat_loss_so_far + heat_loss,
                     straight_steps: self.straight_steps + 1,
                 };
                 next_positions.push(straight_pos);
@@ -45,16 +32,13 @@ impl Pos {
 
         let left_turn = (-self.direction.1, -self.direction.0);
         let next_pos = (self.position.0 + left_turn.0, self.position.1 + left_turn.1);
-        if in_bounds(&self.layout, &next_pos) {
-            let heat_loss: i128 = self.layout[next_pos.0 as usize][next_pos.1 as usize];
+        if in_bounds(layout, &next_pos) {
             let left_pos = Pos {
-                layout: self.layout.to_vec(),
                 position: next_pos,
                 direction: left_turn,
-                heat_loss,
-                heat_loss_so_far: self.heat_loss_so_far + heat_loss,
                 straight_steps: 1,
             };
+
             next_positions.push(left_pos);
         }
 
@@ -63,16 +47,13 @@ impl Pos {
             self.position.0 + right_turn.0,
             self.position.1 + right_turn.1,
         );
-        if in_bounds(&self.layout, &next_pos) {
-            let heat_loss: i128 = self.layout[next_pos.0 as usize][next_pos.1 as usize];
+        if in_bounds(layout, &next_pos) {
             let right_pos = Pos {
-                layout: self.layout.to_vec(),
                 position: next_pos,
                 direction: right_turn,
-                heat_loss,
-                heat_loss_so_far: self.heat_loss_so_far + heat_loss,
                 straight_steps: 1,
             };
+
             next_positions.push(right_pos);
         }
 
@@ -81,7 +62,7 @@ impl Pos {
             .map(|p| {
                 (
                     p.clone(),
-                    p.layout[p.position.0 as usize][p.position.1 as usize],
+                    layout[p.position.0 as usize][p.position.1 as usize],
                 )
             })
             .collect()
@@ -96,18 +77,18 @@ fn in_bounds(height_map: &Vec<Vec<i128>>, position: &(i128, i128)) -> bool {
 }
 
 fn solve_part_one(layout: &Vec<Vec<i128>>) -> i128 {
-    let goal: (i128, i128) = ((layout.len() - 1) as i128, (layout[layout.len() - 1].len() - 1) as i128);
+    let goal: (i128, i128) = (
+        (layout.len() - 1) as i128,
+        (layout[layout.len() - 1].len() - 1) as i128,
+    );
     let start: Pos = Pos {
-        layout: layout.to_vec(),
-        position: (0,0),
-        direction: (0,1),
-        heat_loss: 0,
-        heat_loss_so_far: 0,
-        straight_steps: 1,
+        position: (0, 0),
+        direction: (0, 1),
+        straight_steps: 0,
     };
     let result = astar(
         &start,
-        |p| p.successors(),
+        |p| p.successors(layout),
         |p| p.distance_simple(&goal) / 3,
         |p| p.position == goal,
     );
@@ -156,7 +137,7 @@ mod tests {
     fn day17_input_part_one() {
         let input = get_input("./src/day17/input.txt");
         let sum_part_one = solve_part_one(&input);
-        assert_eq!(7562, sum_part_one);
+        assert_eq!(932, sum_part_one);
     }
 
     #[test]
